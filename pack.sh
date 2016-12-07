2dace5a1309d80e95c5b313fcb46034fe4ea83ba
Einar Ingebrigtsen <einar@dolittle.com>
2016-12-07 14:23:03 +0100

    New version pushed

--- pack.sh
+++ pack.sh
@@ -10,6 +10,6 @@ cp ./Source/bin/Debug/netcoreapp1.0/*.dll ./artefacts/lib/netcoreapp1.0
 cp ./Source/bin/Debug/netcoreapp1.0/*.pdb ./artefacts/lib/netcoreapp1.0
 cp ./Source/Package.nuspec ./artefacts
 cd artefacts
-nuget pack Package.nuspec -symbols -version 1.0.0 -suffix nightly001 
+nuget pack Package.nuspec -symbols -version 1.0.0 -suffix nightly002 
 
 #dotnet pack ./Source -c Release -o artefacts --version-suffix nightly001
