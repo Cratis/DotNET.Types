8b8c3490ae96953ed5b3505578ee5129c40fa7ff
Einar Ingebrigtsen <einar@dolittle.com>
2016-12-09 09:59:33 +0100

    Pushed 1.0.4

--- pack.sh
+++ pack.sh
@@ -10,7 +10,7 @@ cp ./Source/bin/Debug/netstandard1.6/*.dll ./artefacts/lib/netstandard1.6
 cp ./Source/bin/Debug/netstandard1.6/*.pdb ./artefacts/lib/netstandard1.6
 cp ./Source/Package.nuspec ./artefacts
 cd artefacts
-nuget pack Package.nuspec -symbols -version 1.0.3
+nuget pack Package.nuspec -symbols -version 1.0.4
 #-suffix nightly002 
 
 #dotnet pack ./Source -c Release -o artefacts --version-suffix nightly001
